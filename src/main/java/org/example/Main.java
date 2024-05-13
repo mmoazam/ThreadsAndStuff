7014f555d97302438b102951788d0a7dbfaa3df3
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-05-13 15:48:33 +0100

    Exercise05: get an ArrayList<Integers> from the supplied Array of String numbers.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,43 @@
 package org.example;
-import java.util.concurrent.*;
-import java.util.concurrent.TimeUnit;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-    //    CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-    //        try {
-    //            // Introduce a delay of 2 seconds
-    //            TimeUnit.SECONDS.sleep(2);
-    //        } catch (InterruptedException e) {
-    //            Thread.currentThread().interrupt();
-    //            throw new RuntimeException(e);
-    //        }
-    //    }).thenRun(() -> {
-    //        // Print "Hello World!" after the delay
-    //        System.out.println("Hello World!");
-    //    });
-    //
-    //    // Ensure the main thread waits for the asynchronous task to complete
-    //    future.join();
-    //
-        } // end of main
+        String data = "85671 34262 92143 50984 24515 68356 77247 12348 56789 98760";
+        ArrayList<Integer> integers = getIntegerListfromStringArray(data);
 
+
+
+
+    } // end of main
+
+    private static ArrayList<Integer> getIntegerListfromStringArray(String data) {
+        String[] intStringArray = data.split(" ");
+        ArrayList<Integer> ints = new ArrayList<>();
+        int integer = 0;
+        for (String s : intStringArray){
+            try {
+               integer = Integer.parseInt(s);
+                ints.add(integer);
+            } catch (IllegalArgumentException e){
+                e.getStackTrace();
+            }
+        }
+        return ints;
+    }
+
+
+    private static BigInteger calculateFactorial(BigInteger num) {
+        BigInteger result = BigInteger.ONE;
+        for (BigInteger i = BigInteger.ONE; i.compareTo(num) <= 0; i = i.add(BigInteger.ONE)) {
+            result = result.multiply(i);
+        }
+        return result;
+    }
 
 
 }

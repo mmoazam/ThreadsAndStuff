a4c895918c81ccc85c7a14bb38f75847a249a40b
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-05-13 17:13:59 +0100

    CompletableFuture to asynchronously perform a factorial transformation on each element in your List<BigInteger> (using the provided method), printing the resulting list to the console. Consider how you can use .thenApply() & .thenAccept() to accomplish this.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,31 +3,61 @@ package org.example;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
     public static void main(String[] args) {
         String data = "85671 34262 92143 50984 24515 68356 77247 12348 56789 98760";
-        ArrayList<Integer> integers = getIntegerListfromStringArray(data);
+        ArrayList<BigInteger> bigIntegers = getIntegerListfromStringArray(data);
+        long startTime = System.nanoTime();
 
+        CompletableFuture<Void> allFactorialsFuture = calculateFactorialsAsync(bigIntegers)
+                .thenAccept(result -> {
+                    System.out.println("Factorials " + result);
+                });
 
+        allFactorialsFuture.join();
+        // calculateFactorialsAsync(bigIntegers);
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.println("Time using Async " + elapsedTime / 1_000_000_000.0);
 
-
+        startTime = System.nanoTime();
+        factorialTransformationForLoop(bigIntegers);
+        elapsedTime =( System.nanoTime() - startTime);
+        System.out.println("Time using for loop " + elapsedTime/1_000_000_000.0);
     } // end of main
 
-    private static ArrayList<Integer> getIntegerListfromStringArray(String data) {
+    static CompletableFuture<List<BigInteger>> calculateFactorialsAsync(ArrayList<BigInteger> bigIntegers) {
+       return CompletableFuture.supplyAsync(() -> {
+          return bigIntegers.stream()
+                  .map(Main::calculateFactorial)
+                  .toList();
+       });
+    }
+
+    static void factorialTransformationForLoop(ArrayList<BigInteger> bigIntegers) {
+
+        for (BigInteger bi : bigIntegers){
+            var answer = calculateFactorial(bi);
+            System.out.println(answer);
+        }
+    }
+
+    private static ArrayList<BigInteger> getIntegerListfromStringArray(String data) {
         String[] intStringArray = data.split(" ");
-        ArrayList<Integer> ints = new ArrayList<>();
-        int integer = 0;
-        for (String s : intStringArray){
+        ArrayList<BigInteger> bigIntegers = new ArrayList<>();
+        BigInteger integer;
+        for (String s : intStringArray) {
             try {
-               integer = Integer.parseInt(s);
-                ints.add(integer);
-            } catch (IllegalArgumentException e){
+                integer = BigInteger.valueOf(Integer.parseInt(s));
+                bigIntegers.add(integer);
+            } catch (IllegalArgumentException e) {
                 e.getStackTrace();
             }
         }
-        return ints;
+        return bigIntegers;
     }
 
 
